4e7c332359ad82ff91eb8e537a830d550a95179c
Werner <werneralexandre@outlook.com>
2019-08-27 12:11:44 -0300

    Iniciado autenticação

--- planil/urls.py
+++ planil/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
@@ -6,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('DeliveryLogistics.urls')),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
+
